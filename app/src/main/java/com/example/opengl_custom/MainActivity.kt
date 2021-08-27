e5a53bbfe5d42555a20524514a461f0f46f3f798
余栋梁 <yudongliang@okay.cn>
2021-08-27 11:29:48 +0800

    新增 lib库

---
+++ app/src/main/java/com/example/opengl_custom/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.opengl_custom
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.opengl_custom.activity.OpenGLES20Activity
+import com.example.opengl_custom.adapter.MainAdapter
+import com.example.opengl_custom.adapter.MyClickListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val list = arrayListOf(
+        "OpenGLES20Activity"
+
+    )
+    private val mainAdapter = MainAdapter(list).apply {
+        listener = object : MyClickListener {
+            override fun onClick(postion: Int) {
+                when(postion){
+                    0->{customStartActivity(OpenGLES20Activity::class.java)}
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = mainAdapter
+        }
+    }
+
+    private fun customStartActivity(java: Class<*>) =
+            startActivity(Intent(this, java))
+
+}
---
+++ app/src/main/java/com/example/opengl_custom/activity/OpenGLES20Activity.kt
@@ -0,0 +1,12 @@
+package com.example.opengl_custom.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.opengl_custom.R
+
+class OpenGLES20Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_g_l_e_s20)
+    }
+}
